eebff4035a9374e844d0e9e7df3f59a836803773
kaufmjoh <kaufmjoh@oregonstate.edu>
2021-03-16 22:52:38 -0700

    Created vector.h and matrix.h. They are currently empty.

---
+++ matrix.h
@@ -0,0 +1,13 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+using namespace std;
+
+class Matrix
+{
+	private:
+	public:
+}
+
+
+#endif
---
+++ vector.h
@@ -0,0 +1,12 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+using namespace std;
+
+class Vector
+{
+	private:
+	public:
+}
+
+#endif
