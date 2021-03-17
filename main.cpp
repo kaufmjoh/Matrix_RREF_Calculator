22891b17c08776cee3cce6a540781acf2ed1a5e8
kaufmjoh <kaufmjoh@oregonstate.edu>
2021-03-16 22:50:15 -0700

    Created main.cpp. It is empty, except for a program header.

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/********************************************************************************************************************
+ * Program: Matrix Row Reduce Echelon Form Calculator (C++)
+ * Author: John Kaufman
+ * Date of Program Creation: 3/16/2021
+ * Date of Program Competion: Work in Progress
+ *
+ * Description: This program performs a series of elementary row operations on a matrix
+ * 		to transform the matrix into its row redeuced echelon form (RREF)
+ *
+ * Input: Input is provided in 2 phases:
+ * 		First, the user enters the number of vectors in the matrix, and the dimension of the matrix
+ * 		Second, the user enters a number of values corresponding to the number of vectors multiplied by the dimension of the matrix.
+ *
+ * 		(Note on input: Each column corresponds to a vector)
+ *
+ * Output: After translating the matrix to its RREF, the program will output the resulting matrix to the terminal
+***********************************************************************************************************************/
+
+
+//The driving function for the whole program.
+int main()
+{
+}
