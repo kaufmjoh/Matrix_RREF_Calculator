aa231691871f6cd58f5cb1746a4c76612b239f78
kaufmjoh <kaufmjoh@oregonstate.edu>
2021-03-16 23:08:31 -0700

    Added basic private and public attributes to matrix.h and vector.h

--- matrix.h
+++ matrix.h
@@ -1,12 +1,28 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
+#include "./vector.h"
+
 using namespace std;
 
 class Matrix
 {
 	private:
+		int dimension;
+		Vector* vectors;
+
 	public:
+		Matrix();
+		~Matrix();
+
+		void Print_Matrix();
+
+		int get_dimension();
+		Vector* get_vectors();
+
+		void set_dimension(int input_dimension);
+		void set_vectors(Vector* input_vectors);
+		
 }
 
 
